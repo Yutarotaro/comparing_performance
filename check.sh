22b796fcafb4e1d6a4271b67b14e64e40e3e95d1
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2022-06-18 17:33:14 +0900

    modify run command

--- check.sh
+++ check.sh
@@ -2,4 +2,4 @@ g++ main.cpp -o mac
 ./mac
 
 docker build -q -t scd .
-docker run -v /Users/yutaro/2022/class/comparing_performance/:/root scd sh -c "cd /root && g++ main.cpp -o ubuntu && ./ubuntu"
+docker run -v $(pwd):/root scd sh -c "cd /root && g++ main.cpp -o ubuntu && ./ubuntu"
