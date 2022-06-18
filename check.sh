6032051b0a519ef1738558c77ee8cfd54651238c
Yutarotaro <9343783873a@g.ecc.u-tokyo.ac.jp>
2022-06-18 17:59:12 +0900

    docker run -rm

--- check.sh
+++ check.sh
@@ -2,4 +2,4 @@ g++ main.cpp -o host
 ./host host
 
 docker build -q -t scd .
-docker run -v $(pwd):/root scd sh -c "cd /root && g++ main.cpp -o guest && ./guest guest"
+docker run -rm -v $(pwd):/root scd sh -c "cd /root && g++ main.cpp -o guest && ./guest guest"
